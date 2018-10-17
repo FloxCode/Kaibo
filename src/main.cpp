1234d8de31d078088fb7d4ef476bbf645cb21ac4
Florian <fksmail@web.de>
2018-10-18 00:17:53 +0200

    main: WiFi-Setup angefangen
    Signed-off-by: Florian <fksmail@web.de>

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,57 @@
 #define OLED_DATA D2
 
 CombinedDisplay display(OLED_CLK, OLED_DATA, SS_CLK, SS_DIO);
-WiFiAdapter wifi();
-Configuration conf();
+WiFiAdapter wifi;
+Configuration conf;
 
-Interval connectionWaitTime(20000, true);
+// Intervalle
+Interval connectionWaitTime(20000, false);
+Interval messageInterval(4000, false);
 
+// AP-Konstanten
+char ap_ssid[] = "Kaibo";
+char ap_password[] = "KaiboSetup";
+char ap_ip[4] = {8, 2, 6, 6};
+
+char message[80];
 
 void setup() {
+    //Serial.begin(9600);
     display.init();
+    display.showDate(0, 0, 0, 2000, 5, 1, 6, false, true, true);
+    delay(3000);
     // TODO: Bootsequenz
-    // Verbingung zu gespeichertem Netzwerk aufnehmen
-    wifi.connectTo(conf.getSsid(), conf.getPassword());
-    connectionTimeout.start();
-    while(!wifi.isConnected() && !connectionWaitTime.over()){
+    // Verbindung zu gespeichertem Netzwerk aufnehmen
+    strcpy(message, wifi.cm_getSsid().c_str());
+    char lines[2][80] = {"Verbinde mit:", message};
+    messageInterval.start();
+    display.showMessage(lines);
+    while(!messageInterval.over()){
         yield();
     }
+    wifi.cm_reconnect();
+    connectionWaitTime.start();
+    while(!wifi.cm_isConnected()){
+        yield();
+        if(connectionWaitTime.over()){
+            break;
+        }
+    }
     // Wenn Verbindung zu gespeichertem Netzwerk nicht aufgebaut werden konnte in AP-Modus wechseln
-    if(!wifi.isConnected()){
-        // TODO: AP-Modus + Konfiguration
+    if(!wifi.cm_isConnected()){
+        wifi.cm_stop();
+        wifi.apm_start(ap_ssid, ap_password, ap_ip);
+        char lines2[2][80] = {ap_ssid, ap_password};
+        display.showMessage(lines2);
+    }
+    else{
+        char lines3[1][80] = {"Verbunden!"};
+        display.showMessage(lines3);
+        messageInterval.start();
+        while(!messageInterval.over()){
+            yield();
+        }
+        display.clearDisplay();
     }
 }
 
