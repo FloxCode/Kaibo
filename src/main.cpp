280c668c9be37554566420b0a91374a82360a73c
Florian <fksmail@web.de>
2018-11-05 23:50:21 +0100

    Statusmeldungen beim Verbindungsaufbau und ggf. in AP-Mode wechseln und Zugangsdaten anzeigen
    Signed-off-by: Florian <fksmail@web.de>

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,20 @@ WiFiAdapter wifi;
 Configuration conf;
 
 // Intervalle
-Interval connectionWaitTime(20000, false);
+Interval connectionWaitTime(5000, false);
 Interval messageInterval(4000, false);
 
 // AP-Konstanten
-char ap_ssid[] = "Kaibo";
-char ap_password[] = "KaiboSetup";
+char ap_ssid[] = "KAIBO-Setup";
+char ap_password[] = "K418053tup";
 char ap_ip[4] = {8, 2, 6, 6};
 
+// Nachrichten
 char message[80];
-char connectingText[] = "Verbinde mit:";
+char t_connecting[] = "Verbinde mit:";
+char t_NoConnection[] = "Keine Verbindung!";
+char t_ssid[] = "WLAN:";
+char t_password[] = "Passwort:";
 
 void setup() {
     //Serial.begin(9600);
@@ -32,27 +36,31 @@ void setup() {
     delay(3000);
     // TODO: Bootsequenz
     // Verbindung zu gespeichertem Netzwerk aufnehmen
-    strcpy(message, wifi.cm_getSsid().c_str());
-    char *lines[80] = {connectingText, message};
-    messageInterval.start();
-    display.showMessage(lines, 2);
-    while(!messageInterval.over()){
-        yield();
-    }
     wifi.cm_reconnect();
     connectionWaitTime.start();
-    while(!wifi.cm_isConnected()){
+    strcpy(message, wifi.cm_getSsid().c_str());
+    char *lines[80] = {t_connecting, message};
+    messageInterval.start();
+    display.showMessage(lines, 2);
+    while(!(messageInterval.over() && wifi.cm_isConnected())){
         yield();
         if(connectionWaitTime.over()){
             break;
         }
     }
+
     // Wenn Verbindung zu gespeichertem Netzwerk nicht aufgebaut werden konnte in AP-Modus wechseln
     if(!wifi.cm_isConnected()){
+        char *lines4[80] = {t_NoConnection};
+        display.showMessage(lines4, 1);
+        messageInterval.start();
+        while(!messageInterval.over()){
+            yield();
+        }
         wifi.cm_stop();
         wifi.apm_start(ap_ssid, ap_password, ap_ip);
-        char *lines2[80] = {ap_ssid, ap_password};
-        display.showMessage(lines2, 2);
+        char *lines2[80] = {t_ssid, ap_ssid, t_password, ap_password};
+        display.showMessage(lines2, 4);
     }
     else{
         char *lines3[80] = {"Verbunden!"};
